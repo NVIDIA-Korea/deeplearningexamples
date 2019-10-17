d99876254c43de974d4ad9744c1fe052901675ab
Jack Han <haanjack@gmail.com>
2019-10-18 02:04:46 +0900

    Switched default dockerfile which uses TensorRT 5

--- FasterTransformer/scripts/docker/build.sh
+++ FasterTransformer/scripts/docker/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-CONT=${1:-"nvcr.io/nvidian/sae/jahan:bert-cuda10.0-trt6"}
+CONT=${1:-"nvcr.io/nvidian/sae/jahan:bert-cuda10.0-trt5"}
 
-docker build . -t ${CONT} -f Dockerfile.trt6_cuda10
+docker build . -t ${CONT} -f Dockerfile.trt5_cuda10
+
+docker tag ${CONT} hanjack/bert:cuda10.0-trt5
 
 # docker push ${CONT}
