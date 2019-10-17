f99443f89fd82200a90e4ba9112f684d3852cd71
Jack Han <haanjack@gmail.com>
2019-10-18 02:05:38 +0900

    Added missing file which shows welcome message for the container launch

---
+++ FasterTransformer/nvidia_entrypoint.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+cat <<EOF
+
+=============================
+== BERT TensorRT Inference ==
+=============================
+
+NVIDIA Release ${NVIDIA_TENSORFLOW_VERSION} (build ${NVIDIA_BUILD_ID})
+TensorFlow Version ${TENSORFLOW_VERSION}
+
+Container image Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+Copyright 2017-2019 The TensorFlow Authors.  All rights reserved.
+
+Various files include modifications (c) NVIDIA CORPORATION.  All rights reserved.
+NVIDIA modifications are covered by the license terms that apply to the underlying project or file.
+EOF
+
+if [[ "$(find /usr -name libcuda.so.1 | grep -v "compat") " == " " || "$(ls /dev/nvidiactl 2>/dev/null) " == " " ]]; then
+  echo
+  echo "WARNING: The NVIDIA Driver was not detected.  GPU functionality will not be available."
+  echo "   Use 'nvidia-docker run' to start this container; see"
+  echo "   https://github.com/NVIDIA/nvidia-docker/wiki/nvidia-docker ."
+else
+  # ( /usr/local/bin/checkSMVER.sh )
+  DRIVER_VERSION=$(sed -n 's/^NVRM.*Kernel Module *\([0-9.]*\).*$/\1/p' /proc/driver/nvidia/version 2>/dev/null || true)
+  if [[ ! "$DRIVER_VERSION" =~ ^[0-9]*.[0-9]*(.[0-9]*)?$ ]]; then
+    echo "Failed to detect NVIDIA driver version."
+  elif [[ "${DRIVER_VERSION%%.*}" -lt "${CUDA_DRIVER_VERSION%%.*}" ]]; then
+    if [[ "${_CUDA_COMPAT_STATUS}" == "CUDA Driver OK" ]]; then
+      echo
+      echo "NOTE: Legacy NVIDIA Driver detected.  Compatibility mode ENABLED."
+    else
+      echo
+      echo "ERROR: This container was built for NVIDIA Driver Release ${CUDA_DRIVER_VERSION%.*} or later, but"
+      echo "       version ${DRIVER_VERSION} was detected and compatibility mode is UNAVAILABLE."
+      echo
+      echo "       [[${_CUDA_COMPAT_STATUS}]]"
+      sleep 2
+    fi
+  fi
+fi
+
+if ! cat /proc/cpuinfo | grep flags | sort -u | grep avx >& /dev/null; then
+  echo
+  echo "ERROR: This container was built for CPUs supporting at least the AVX instruction set, but"
+  echo "       the CPU detected was $(cat /proc/cpuinfo |grep "model name" | sed 's/^.*: //' | sort -u), which does not report"
+  echo "       support for AVX.  An Illegal Instrution exception at runtime is likely to result."
+  echo "       See https://en.wikipedia.org/wiki/Advanced_Vector_Extensions#CPUs_with_AVX ."
+  sleep 2
+fi
+
+echo
+if [[ $# -eq 0 ]]; then
+  exec "/bin/bash"
+else
+  exec "$@"
+fi
